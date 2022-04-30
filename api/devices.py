79adace7fdf8515b493198ca6297fd4cd24b2481
Sunbrye Ly <sunbryely@Sunbryes-MacBook-Pro.local>
2022-04-29 18:28:41 -0700

    working write_point

--- api/devices.py
+++ api/devices.py
@@ -1,22 +1,45 @@
 from datetime import datetime
 
+import configparser
 import os
 import urllib3
+from uuid import uuid4
 from influxdb_client import Authorization, InfluxDBClient, Permission, PermissionResource, Point, WriteOptions
 from influxdb_client.client.query_api import QueryOptions
 from influxdb_client.client.authorizations_api import AuthorizationsApi
 from influxdb_client.client.bucket_api import BucketsApi
 from influxdb_client.client.query_api import QueryApi
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 # Set up configuration file needed to reach influxdb
-config = {
-    "id": os.getenv("VIRTUAL_DEVICE_NAME"),
-    "influx_url": os.getenv("INFLUX_URL"),
-    "influx_token": os.getenv("INFLUX_TOKEN"),
-    "influx_org": os.getenv("INFLUX_ORG"),
-    "influx_bucket": os.getenv("INFLUX_BUCKET"),
-    "influx_bucket_auth": os.getenv("INFLUX_BUCKET_AUTH")
-}
+# config = {
+#     "id": os.getenv("VIRTUAL_DEVICE_NAME"),
+#     "influx_url": os.getenv("INFLUX_URL"),
+#     "influx_token": os.getenv("INFLUX_TOKEN"),
+#     "influx_org": os.getenv("INFLUX_ORG"),
+#     "influx_bucket": os.getenv("INFLUX_BUCKET"),
+#     "influx_bucket_auth": os.getenv("INFLUX_BUCKET_AUTH")
+# }
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+def get_config():
+    print(config.get('APP', 'INFLUX_URL'))
+    return config
+
+
+def get_buckets():
+    influxdb_client = InfluxDBClient(url=config.get('APP', 'INFLUX_URL'),
+                                     token=config.get('APP', 'INFLUX_TOKEN'),
+                                     org=config.get('APP', 'INFLUX_ORG'))
+
+    buckets_api = influxdb_client.buckets_api()
+    buckets = buckets_api.find_buckets()
+    return buckets
+
 
 
 # this checks if the device is already in the auth bucket
@@ -29,23 +52,45 @@ config = {
 #  @returns promise with an Record<deviceId, {deviceId, createdAt, updatedAt, key, token}>.
 
 def get_device(device_id) -> {}:
-    influxdb_client = InfluxDBClient(url=config['influx_url'],
-                                     token=config['influx_token'],
-                                     org=config['influx_org'])
+    influxdb_client = InfluxDBClient(url=config.get('APP', 'INFLUX_URL'),
+                                     token=config.get('APP', 'INFLUX_TOKEN'),
+                                     org=config.get('APP', 'INFLUX_ORG'))
 
+    # fix this
+    device_id = uuid4()
     query_api = QueryApi(influxdb_client)
 
-    device_filter = ''
-    flux_query = f"from(bucket: {config['influx_bucket_auth']}) " \
+
+    device_filter = f"deviceId !== undefined ? flux` and r.deviceId == '${device_id}'` : flux` and r._field != 'token'"
+    flux_query = f"from(bucket: {config.get('APP', 'INFLUX_BUCKET_AUTH')}) " \
                  f"|> range(start: 0) " \
                  f"|> filter(fn: (r) => r._measurement == 'deviceauth'{device_filter}) " \
                  f"|> last()"
     devices = {}
+
+    query = ' from(bucket:"my-bucket")\
+        |> range(start: -60m)\
+        |> filter(fn:(r) => r._measurement == "deviceauth") '
     print(f"*** QUERY *** \n {flux_query}")
     # TODO FIX
     query_api.query(flux_query)
     return {}
 
+
+def test_create_device():
+    influxdb_client = InfluxDBClient(url=config.get('APP', 'INFLUX_URL'),
+                                     token=config.get('APP', 'INFLUX_TOKEN'),
+                                     org=config.get('APP', 'INFLUX_ORG'))
+
+    device_id = str(uuid4())
+    created_at = str(datetime.now())
+    write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
+
+    point = Point('deviceauth').tag("deviceId", device_id).\
+        field('key', 'fake_auth_id').field('token', 'fake_auth_token')
+    client_response = write_api.write(bucket=config.get('APP', 'INFLUX_BUCKET_AUTH'), record=point)
+
+    return client_response
 
 # Creates an authorization for a deviceId and writes it to a bucket
 def create_device(device_id) -> Authorization:
@@ -57,16 +102,16 @@ def create_device(device_id) -> Authorization:
     else:
         print(f"createDeviceAuthorization: deviceId ={device_id}")
         authorization = create_authorization(device_id)
-        influxdb_client = InfluxDBClient(url=config['influx_url'],
-                                         token=config['influx_token'],
-                                         org=config['influx_org'])
+        influxdb_client = InfluxDBClient(url=config.get('APP', 'INFLUX_URL'),
+                                         token=config.get('APP', 'INFLUX_TOKEN'),
+                                         org=config.get('APP', 'INFLUX_ORG'))
 
         write_api = influxdb_client.write_api(write_options=WriteOptions(batch_size=1))
         point = Point("deviceauth") \
             .tag("deviceId", device_id) \
             .field("key", authorization.id) \
             .field("token", authorization.token)
-        write_api.write(bucket=config["influx_bucket_auth"], record=point)
+        write_api.write(bucket=config.get('APP', 'INFLUX_BUCKET_AUTH'), record=point)
         write_api.close()
         return authorization
 
@@ -75,20 +120,22 @@ def create_device(device_id) -> Authorization:
 # Function should return a response code
 # Creates an authorization for a supplied deviceId
 def create_authorization(device_id) -> Authorization:
-    influxdb_client = InfluxDBClient(url=config['influx_url'],
-                                     token=config['influx_token'],
-                                     org=config['influx_org'])
+    influxdb_client = InfluxDBClient(url=config.get('APP', 'INFLUX_URL'),
+                                     token=config.get('APP', 'INFLUX_TOKEN'),
+                                     org=config.get('APP', 'INFLUX_ORG'))
+
     authorization_api = AuthorizationsApi(influxdb_client)
 
     buckets_api = BucketsApi(influxdb_client)
-    bucket = buckets_api.find_bucket_by_name(config['influx_bucket']) # function returns only 1 bucket
+    buckets = buckets_api.find_bucket_by_name(config.get('APP', 'INFLUX_BUCKET_AUTH')) # function returns only 1 bucket
+    bucket_id = buckets.buckets[0].id
     desc_prefix = 'IoTCenterDevice: '
     # get bucket_id from bucket
-    org_resource = PermissionResource(org_id=config['influx_org'], type="orgs")
+    org_resource = PermissionResource(org_id=config.get('APP', 'INFLUX_ORG'), type="orgs")
     read = Permission(action="read", resource=org_resource)
     write = Permission(action="write", resource=org_resource)
     permissions = [read, write]
-    authorization = Authorization(org_id=config['influx_org'],
+    authorization = Authorization(org_id=config.get('APP', 'INFLUX_ORG'),
                                   permissions=permissions,
                                   description=desc_prefix + device_id)
     request = authorization_api.create_authorization(authorization)
--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from api.helper_functions import get_current_time
-from api import devices
+from api import devices, write_data
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
@@ -23,8 +23,44 @@ def get_devices():
     return render_template('devices.html', device=response.device_id)
 
 
+@app.route('/create')
+def create_device():
+    response = devices.test_create_device()
+    return render_template('create.html', response=response)
+
+
+@app.route('/buckets')
+def get_buckets():
+    buckets = devices.get_buckets()
+    buckets = buckets.buckets
+    # list of Bucket
+    # bucket['name']
+    # {'created_at': datetime.datetime(2022, 3, 15, 17, 22, 33, 731038, tzinfo=tzutc()),
+    #  'description': None,
+    #  'id': '11939869ae5f7415',
+    #  'labels': [],
+    #  'links': {'_self': '/api/v2/buckets/11939869ae5f7415',
+    #            'labels': '/api/v2/buckets/11939869ae5f7415/labels',
+    #            'members': '/api/v2/buckets/11939869ae5f7415/members',
+    #            'org': '/api/v2/orgs/bea7ea952287f70d',
+    #            'owners': '/api/v2/buckets/11939869ae5f7415/owners',
+    #            'write': '/api/v2/write?org=bea7ea952287f70d&bucket=11939869ae5f7415'},
+    #  'name': "sly's Bucket",
+    #  'org_id': 'bea7ea952287f70d',
+    #  'retention_rules': [{'every_seconds': 2592000,
+    #                       'shard_group_duration_seconds': None,
+    #                       'type': 'expire'}],
+    #  'rp': None,
+    #  'schema_type': 'implicit',
+    #  'type': 'user',
+    #  'updated_at': datetime.datetime(2022, 3, 15, 17, 22, 33, 731038, tzinfo=tzutc())}
+    # parse the response
+    return render_template('buckets.html', buckets=buckets)
+
+
 @app.route('/test')
 def test():
-    return "<p> Hello World </p>"
+    # buckets = devices.get_buckets()
+    # return config.get('APP', 'INFLUX_URL')
 
-
+    return render_template('help.html')
