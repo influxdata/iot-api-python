c6ef8375b1d7f412cabdeb7906270de5926627d2
Jason Stirnaman <jstirnaman@influxdata.com>
2022-06-09 09:51:25 -0500

    fix: Cleanup API routes
    - use POST /devices instead of /devices/create
    - remove unused /devices/authorizations

--- app.py
+++ app.py
@@ -81,32 +81,15 @@ def data():
 
 # /api routes
 
-
-@app.route('/api')
-def api_get_routes():
-    if request.method == "OPTIONS": # CORS preflight
-        print('preflight')
-        return _build_cors_preflight_response()
-    routes = []
-    return _corsify_actual_response(jsonify(routes))
-
-
-@app.route('/api/devices/<string:device_id>', methods=['GET', 'POST'])
-def api_get_device(device_id):
+@app.route('/api/buckets')
+def api_api_get_buckets():
     if request.method == "OPTIONS": # CORS preflight
         return _build_cors_preflight_response()
-    return _corsify_actual_response(jsonify(devices.get_device(device_id)))
+    buckets = devices.get_buckets()
+    buckets = buckets.buckets
+    return _corsify_actual_response(jsonify(buckets))
 
-
-@app.route('/api/devices', methods=['GET', 'POST'])
-def api_get_devices():
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
-    result = devices.get_device(device_id=None)
-    data = json.dumps(result, cls=FluxStructureEncoder, indent=2)
-    return _corsify_actual_response(make_response(data))
-
-
+    
 @app.route('/api/devices/<string:device_id>/measurements', methods=['POST'])
 def api_get_measurements(device_id):
     if request.method == "OPTIONS": # CORS preflight
@@ -120,42 +103,40 @@ def api_get_measurements(device_id):
     return _corsify_actual_response(response)
 
 
+@app.route('/api/devices/<string:device_id>', methods=['GET', 'POST'])
+def api_get_device(device_id):
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    return _corsify_actual_response(jsonify(devices.get_device(device_id)))
+
+
 @app.route('/api/devices/generate', methods=['POST'])
 def api_generate_data():
     if request.method == "OPTIONS": # CORS preflight
         return _build_cors_preflight_response()
-    print(request.json['deviceIds'])
     data = devices.write_measurements(request.json['deviceIds'])
     return _corsify_actual_response(jsonify(data))
 
 
-@app.route('/api/devices/create', methods=['POST'])
+@app.route('/api/devices', methods=['GET'])
+def api_get_devices():
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    result = devices.get_device(device_id=None)
+    data = json.dumps(result, cls=FluxStructureEncoder, indent=2)
+    return _corsify_actual_response(make_response(data))
+
+
+@app.route('/api/devices', methods=['POST'])
 def api_create_device():
     if request.method == "OPTIONS": # CORS preflight
         return _build_cors_preflight_response()
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        json = request.json
-        return json
+        device_id = devices.create_device(request.json['deviceId'])
+        return _corsify_actual_response(jsonify({ 'deviceId': device_id}))
     else:
         return 'Content-Type not supported!'
-    device_id = devices.create_device(request.json.device_id)
-    return _corsify_actual_response(jsonify({ 'deviceId': device_id}))
-
-
-@app.route('/api/buckets')
-def api_api_get_buckets():
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
-    buckets = devices.get_buckets()
-    buckets = buckets.buckets
-    return _corsify_actual_response(jsonify(buckets))
-
-
-@app.route('/api/authorizations/create', methods=['POST'])
-def api_auth():
-    response = devices.create_authorization('test_id')
-    return _corsify_actual_response(jsonify(response))
 
 
 def _build_cors_preflight_response():
