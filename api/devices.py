d389a0e072c7a03dfea99e5663bdc32be94966bb
Jason Stirnaman <stirnamanj@gmail.com>
2022-06-15 15:45:43 -0500

    Feat/common UI (#8)
    * fix: Retrieve a specific device or all devices.
    
    * docs: Start Flask on localhost:5200 to avoid localhost:5000 conflicts on macOS.
    
    * feat: Add /api routes.
    
    * fix: Routes and Flux results.
    
    - Make routes consistent and RESTful.
    - Return Flux CSV for device measurements.
    
    * fix: Return measurements CSV for fromFlux() in UI.
    
    * docs: Cleanup terminal commands.
    
    * fix: Cleanup API routes
    
    - use POST /devices instead of /devices/create
    - remove unused /devices/authorizations
    
    * docs: env variable instruction.
    
    * fix: Ignore /virtualenv
    
    * docs: Update README - fixes and numbered steps.
    
    * Update app.py
    
    Co-authored-by: sunbryely-influxdata <101659702+sunbryely-influxdata@users.noreply.github.com>
    
    * fix: Create device authorization.
    
    - Limit to INFLUX_BUCKET
    - Add description to authorization
    
    Co-authored-by: sunbryely-influxdata <101659702+sunbryely-influxdata@users.noreply.github.com>

--- api/devices.py
+++ api/devices.py
@@ -1,15 +1,18 @@
 from cgi import print_environ
 import configparser
+import json
 import os
 from datetime import datetime
 from uuid import uuid4
 from influxdb_client import Authorization, InfluxDBClient, Permission, PermissionResource, Point, WriteOptions
 from influxdb_client.client.authorizations_api import AuthorizationsApi
 from influxdb_client.client.bucket_api import BucketsApi
+from influxdb_client.client.flux_table import FluxStructureEncoder
 from influxdb_client.client.query_api import QueryApi
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 from api.sensor import Sensor
+from influxdb_client.domain.dialect import Dialect
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -24,25 +27,35 @@ def get_buckets():
     return buckets
 
 
-def get_device(device_id) -> {}:
+def get_device(device_id=None) -> {}:
     influxdb_client = InfluxDBClient(url=config.get('APP', 'INFLUX_URL'),
                                      token=os.environ.get('INFLUX_TOKEN'),
                                      org=os.environ.get('INFLUX_ORG'))
     # Queries must be formatted with single and double quotes correctly
     query_api = QueryApi(influxdb_client)
-    device_id = str(device_id)
-    device_filter = f'r.deviceId == "{device_id}" and r._field != "token"'
+    device_filter = ''
+    if device_id:
+        device_id = str(device_id)
+        device_filter = f'r.deviceId == "{device_id}" and r._field != "token"'
+    else:
+        device_filter = f'r._field != "token"'
+
     flux_query = f'from(bucket: "{config.get("APP", "INFLUX_BUCKET_AUTH")}") ' \
                  f'|> range(start: 0) ' \
                  f'|> filter(fn: (r) => r._measurement == "deviceauth" and {device_filter}) ' \
                  f'|> last()'
-
+    
     response = query_api.query(flux_query)
-    results = []
+    result = []
     for table in response:
         for record in table.records:
-            results.append((record.get_field(), record.get_value()))
-    return results
+            try:
+                'updatedAt' in record
+            except KeyError:
+                record['updatedAt'] = record.get_time()
+                record[record.get_field()] = record.get_value()
+            result.append(record.values)
+    return result
 
 
 def create_device(device_id=None):
@@ -69,8 +82,12 @@ def create_device(device_id=None):
     # Return None on failure
     return None
 
+def write_measurements(device_ids):
+    for device_id in device_ids:
+        print(f"Writing measurements for: {device_id}")
+        write_measurement(device_id)
 
-def write_measurements(device_id):
+def write_measurement(device_id):
     influxdb_client = InfluxDBClient(url=config.get('APP', 'INFLUX_URL'),
                                      token=os.environ.get('INFLUX_TOKEN'),
                                      org=os.environ.get('INFLUX_ORG'))
@@ -102,28 +119,25 @@ def write_measurements(device_id):
     return None
 
 
-def get_measurements(device_id):
+def get_measurements(query):
     influxdb_client = InfluxDBClient(url=config.get('APP', 'INFLUX_URL'),
                                      token=os.environ.get('INFLUX_TOKEN'),
                                      org=os.environ.get('INFLUX_ORG'))
 
     # Queries must be formatted with single and double quotes correctly
     query_api = QueryApi(influxdb_client)
-    device_id = str(device_id)
-    device_filter = f'r.device == "{device_id}"'
-    flux_query = f'from(bucket: "{config.get("APP", "INFLUX_BUCKET")}") ' \
-                 f'|> range(start: 0) ' \
-                 f'|> filter(fn: (r) => r._measurement == "environment" and {device_filter}) ' \
-                 f'|> last()'
-
-    response = query_api.query(flux_query)
-
-    # iterate through the result(s)
-    results = []
-    for table in response:
-        results.append(table.records[0].values)
-
-    return results
+    result = query_api.query_csv(query,
+                                   dialect=Dialect(
+                                       header=True,
+                                       delimiter=",",
+                                       comment_prefix="#",
+                                       annotations=['group', 'datatype', 'default'],
+                                       date_time_format="RFC3339"))
+    
+    response = ''
+    for row in result:
+        response += (',').join(row) + ('\n')
+    return response
 
 # TODO
 # Function should return a response code
@@ -134,18 +148,18 @@ def create_authorization(device_id) -> Authorization:
                                      org=os.environ.get('INFLUX_ORG'))
 
     authorization_api = AuthorizationsApi(influxdb_client)
-
+    # get bucket_id from bucket
     buckets_api = BucketsApi(influxdb_client)
-    buckets = buckets_api.find_bucket_by_name(config.get('APP', 'INFLUX_BUCKET_AUTH'))  # function returns only 1 bucket
+    buckets = buckets_api.find_bucket_by_name(config.get('APP', 'INFLUX_BUCKET'))  # function returns only 1 bucket
     bucket_id = buckets.id
     org_id = buckets.org_id
     desc_prefix = f'IoTCenterDevice: {device_id}'
-    # get bucket_id from bucket
-    org_resource = PermissionResource(org_id=org_id, type="buckets")
+    org_resource = PermissionResource(org_id=org_id, id=bucket_id, type="buckets")
     read = Permission(action="read", resource=org_resource)
     write = Permission(action="write", resource=org_resource)
     permissions = [read, write]
-    request = authorization_api.create_authorization(org_id=org_id, permissions=permissions)
+    authorization = Authorization(org_id=org_id, permissions=permissions, description=desc_prefix)
+    request = authorization_api.create_authorization(authorization)
     return request
 
 
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response
 from api.helper_functions import get_current_time
 from api import devices
+import json
+from influxdb_client.client.flux_table import FluxStructureEncoder
 
 app = Flask(__name__, template_folder='templates')
 
@@ -10,9 +12,18 @@ def index():
     return render_template('index.html', time=get_current_time())
 
 
-@app.route('/devices', methods=['GET', 'POST'])
+@app.route('/device/<string:device_id>', methods=['GET', 'POST'])
+def get_device(device_id):
+    result = devices.get_device(device_id)
+    if not result:
+        return render_template('devices.html', data=None)
+    else:
+        return render_template('devices.html', data=result)
+
+
+@app.route('/devices/', methods=['GET', 'POST'])
 def get_devices():
-    result = devices.get_device()
+    result = devices.get_device(device_id=None)
     if not result:
         return render_template('devices.html', data=None)
     else:
@@ -66,3 +77,76 @@ def data():
         device_id = request.form.get('device_id_input', None)
         results = devices.get_measurements(device_id)
         return render_template('data.html', data=results)
+
+
+# /api routes
+
+@app.route('/api/buckets')
+def api_get_buckets():
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    buckets = devices.get_buckets()
+    buckets = buckets.buckets
+    return _corsify_actual_response(jsonify(buckets))
+
+    
+@app.route('/api/devices/<string:device_id>/measurements', methods=['POST'])
+def api_get_measurements(device_id):
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    query = request.get_json()['query']
+    if query is not None:
+      data = devices.get_measurements(query)
+      response = make_response(data, 200, {'content-type': 'application/csv'})
+    else:
+      response = make_response('Request is missing query', 404)  
+    return _corsify_actual_response(response)
+
+
+@app.route('/api/devices/<string:device_id>', methods=['GET', 'POST'])
+def api_get_device(device_id):
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    return _corsify_actual_response(jsonify(devices.get_device(device_id)))
+
+
+@app.route('/api/devices/generate', methods=['POST'])
+def api_generate_data():
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    data = devices.write_measurements(request.json['deviceIds'])
+    return _corsify_actual_response(jsonify(data))
+
+
+@app.route('/api/devices', methods=['GET'])
+def api_get_devices():
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    result = devices.get_device(device_id=None)
+    data = json.dumps(result, cls=FluxStructureEncoder, indent=2)
+    return _corsify_actual_response(make_response(data))
+
+
+@app.route('/api/devices', methods=['POST'])
+def api_create_device():
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        device_id = devices.create_device(request.json['deviceId'])
+        return _corsify_actual_response(jsonify({ 'deviceId': device_id}))
+    else:
+        return 'Content-Type not supported!'
+
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
